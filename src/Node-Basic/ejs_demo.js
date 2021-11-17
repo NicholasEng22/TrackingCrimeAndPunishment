7bad93dff535224cfa95d54a60e21b121b249a73
NicholasEng22 <61562438+NicholasEng22@users.noreply.github.com>
2021-11-16 21:57:17 -0500

    Added index.html

--- src/Node-Basic/ejs_demo.js
+++ src/Node-Basic/ejs_demo.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const ejs = require('ejs');
 
-let state_info = fs.readFileSync('../resources/IncarcerationData.json' "utf8");
-let index_template = fs.readFileSync('views/index.ejs', 'utf8');
+let state_info = fs.readFileSync('../../resources/IncarcerationData.json' "utf8");
+let index_template = fs.readFileSync('index.ejs', 'utf8');
 
 let index_html = ejs.render(index_template, {
-  filename: __dirname + '/views/index.ejs',
+  filename: __dirname + 'index.ejs',
   data: JSON.parse(state_info)
 });
 
-fs.writeFileSync('../index.html', index_html, 'utf8');
+fs.writeFileSync('index.html', index_html, 'utf8');
