3344f3fb975bbb741501b4bb0319b4652292d388
ari-sussman1186 <ari.sussman22@trinityschoolnyc.org>
2021-11-17 09:24:30 -0500

    Created MacroSummary

--- src/Node-Basic/ejs_macro.js
+++ src/Node-Basic/ejs_macro.js
@@ -1,22 +1,23 @@
 const fs = require('fs');
 const ejs = require('ejs');
 
-let state_info = fs.readFileSync('../../resources/IncarcerationData.json' "utf8");
+let state_info = fs.readFileSync('../../resources/IncarcerationData.json', 'utf8');
 let index_template = fs.readFileSync('index.ejs', 'utf8');
 
-let macroSummaryTable = {};
+let stateJSON = JSON.parse(state_info);
 
-state_info.forEach(function(state) {
+let macroSummaryTable = [];
+
+stateJSON.forEach(function(state) {
   if ((state["year"] == "year") || state["year"] == "2016") {
     macroSummaryTable.push(state);
   }
 });
 
 
-
 let index_html = ejs.render(index_template, {
   filename: __dirname + 'index.ejs',
-  data: JSON.parse(state_info)
+  data: macroSummaryTable
 });
 
 fs.writeFileSync('ejsMacro.html', index_html, 'utf8');
