659211e14c71c103b2080b6e4a321e99de500b1e
ari-sussman1186 <ari.sussman22@trinityschoolnyc.org>
2021-11-18 09:04:15 -0500

    Update, Completed micropage test at ejs_micro.html

--- src/ejs_micro.js
+++ src/ejs_micro.js
@@ -1,12 +1,23 @@
 const fs = require('fs');
 const ejs = require('ejs');
 
-let state_info = fs.readFileSync('../../resources/IncarcerationData.json' "utf8");
-let index_template = fs.readFileSync('index.ejs', 'utf8');
+let state_info = fs.readFileSync('../resources/IncarcerationData.json', 'utf8');
+let index_template = fs.readFileSync('views/ejsMicro.ejs', 'utf8');
 
-let index_html = ejs.render(index_template, {
-  filename: __dirname + 'index.ejs',
-  data: JSON.parse(state_info)
+let stateJSON = JSON.parse(state_info);
+
+let microSummaryTable = [];
+
+stateJSON.forEach(function(state) {
+  if ((state["jurisdiction"] == "jurisdiction") || state["jurisdiction"] == "ALABAMA") {
+    microSummaryTable.push(state);
+  }
 });
 
-fs.writeFileSync('index.html', index_html, 'utf8');
+
+let index_html = ejs.render(index_template, {
+  filename: __dirname + '/views/ejsMicro.ejs',
+  data: microSummaryTable
+});
+
+fs.writeFileSync('ejsMicro.html', index_html, 'utf8');
