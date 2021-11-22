49c24c6f35bec55be9ccb769cdfe83de187d642a
ari-sussman1186 <ari.sussman22@trinityschoolnyc.org>
2021-11-22 15:12:17 -0500

    Update: Begin constructing project pipeline: setup.js creates build folder and populate static html files

---
+++ src/setup.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+fs.rmdirSync("../build", {recursive: true});
+
+fs.mkdirSync("../build");
+
+
+fs.copyFileSync("about.html", "../build/about.html");
+fs.copyFileSync("test.html", "../build/test.html");
+fs.copyFileSync("index.html", "../build/index.html");
