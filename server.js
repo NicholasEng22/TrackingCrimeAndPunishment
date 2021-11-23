41964d969a11460ebf09515a016cb597f93b45ce
ari-sussman1186 <ari.sussman22@trinityschoolnyc.org>
2021-11-23 10:55:30 -0500

    p

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ghp = require("gh-pages");
 
 const app = express();
 const port = 3000;
@@ -8,3 +9,5 @@ app.use(express.static('build')); //greenlights all requests for files in the st
 app.listen(port, () => {
   console.log(`Data Journalism at http://localhost:${port}`)
 });
+
+//ghp.publish("build", function(err) {});
