20f522dbf11645320c15a9c3eab4df5d0057af1c
ari-sussman1186 <ari.sussman22@trinityschoolnyc.org>
2021-12-06 15:20:37 -0500

    Push Poosh

--- src/setup.js
+++ src/setup.js
@@ -8,3 +8,5 @@ fs.mkdirSync("build/src");
 
 fs.copyFileSync("src/plotlyGenerator.js", "build/src/plotlyGenerator.js");
 fs.copyFileSync("resources/crime_and_incarceration_by_state_data.csvV2.csv", "build/src/crime_and_incarceration_by_state_data.csvV2.csv");
+
+fs.mkdirSync("build/img");
